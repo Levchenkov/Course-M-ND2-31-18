ad601d942a9491f1c966b9d7f9f7124bc344c25d
akaravitski <akaravitski@servicechannel.com>
2018-12-30 16:54:44 +0300

    Added lab 7 from Korovitskiy

---
+++ Korovitskiy/Lab7/JWT/Controllers/AuthorizeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JWT.Models;
+using JWT.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWT.Controllers
+{
+    public class AuthorizeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Authorize()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            user.Role = "";
+            UserRepository.Users.Add(user);
+            return RedirectToAction("Authorize");
+        }
+
+        [HttpPost("/token")]
+        public async Task Token(User user)
+        {
+            var identity = GetIdentity(user.Login, user.Password);
+            if (identity == null)
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("Invalid username or password.");
+            }
+
+            var now = DateTime.UtcNow;
+
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            Response.ContentType = "application/json";
+            await Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User person = UserRepository.Users.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Korovitskiy/Lab7/JWT/Repository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using JWT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Repository
+{
+    public class UserRepository
+    {
+        public static IList<User> Users = new List<User>
+        {
+            new User {Login="admin", Password="123", Role = "admin" },
+            new User { Login="qwerty", Password="55555", Role = "user" }
+        };
+    }
+}
