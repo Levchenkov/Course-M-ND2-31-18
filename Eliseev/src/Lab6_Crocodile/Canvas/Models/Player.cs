eaadb779ce5648e2355a63ad2858fe1466089e7c
TheDarkKnight8888 <ant_9393@bk.ru>
2019-01-26 19:16:53 +0300

    crocodile

---
+++ Eliseev/src/Lab6_Crocodile/Canvas/Models/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Canvas.Models
+{
+    public class Player
+    {
+        public string ConnectionId
+        {
+            get;
+            set;
+        }
+
+        public string RoomId
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Role
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Eliseev/src/Lab6_Crocodile/Canvas/Models/Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Canvas.Models
+{
+    public class Room
+    {
+        private List<Player> players;
+
+        public Room()
+        {
+            players = new List<Player>();
+        }
+
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Task
+        {
+            get;
+            set;
+        }
+
+        public List<Player> Players
+        {
+            get
+            {
+                return players;
+            }            
+        }
+    }
+}
